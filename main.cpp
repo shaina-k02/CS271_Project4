0f313ef35cdaefbfb9b294b2c97e121c69a52669
anniepfeufer <pfeufe_a1@denison.edu>
2023-03-10 15:55:39 -0500

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 
 
 #include <iostream>
-#include "bst.h"
+#include "usecase.cpp"
 using namespace std;
 
 int main(){
+    BST<string,int>* bst = create_bst();
+    string s= convert(bst);
     return 0;
 }
--- usecase.cpp
+++ usecase.cpp
@@ -10,16 +10,21 @@ using namespace std;
 
 using namespace std;
 
-int main() {
+
+BST<string,int>* create_bst() {
     // Read the txt file and build the BST
-    BST bst;
+    BST<string,int>* bst= new BST<string,int>;
     ifstream file("binhex.txt");
     string bin, hex;
 
     while (file >> bin >> hex) {
-        bst.insert(bin, hex[0]);
+        bst->insert(bin, hex[0]);
     }
+    return bst;
+}
 
+
+string convert(BST<string,int>* bst) {
     // Prompt the user for a binary value and convert it to hex
     string binary;
     cout << "Enter binary representation for conversion:" << endl;
@@ -28,11 +33,13 @@ int main() {
     string hex_result = "";
     for (int i = 0; i < binary.length(); i += 4) {
         string sub_bin = binary.substr(i, 4);
-        char hex_char = bst.search(sub_bin);
-        hex_result += hex_char;
+        int isub=stoi(sub_bin);
+        string hex_str = bst->get(isub);
+        hex_result += hex_str;
     }
 
     // Display the result
     cout << "Hexadecimal representation of " << binary << " is 0x" << hex << hex_result << endl;
 
-    return
+    return binary;
+}
